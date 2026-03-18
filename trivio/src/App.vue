5b7e38f33ffaef201860898b51170e2a20188a87
Victor Nogueira <vgdsno@gmail.com>
2026-03-17 23:10:54 -0300

    feat: setup inicial do projeto

---
+++ trivio/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/shared/components/ui/button'
+</script>
+
+
+<template>
+  <div class="h-screen flex flex-col items-center justify-center gap-4 bg-gray-100">
+    <Button>Exemplo</Button>
+  </div>
+</template>
+
+<style scoped></style>
